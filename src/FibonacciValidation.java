5be527e79d69aa92a747df7a45bf1ff22500a273
Willyan Josué Bastos Silva <willyanbastos@live.com>
2024-09-25 16:14:57 -0300

    FibonacciValidation

---
+++ src/FibonacciValidation.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FibonacciValidation {
+    public static Boolean isFibonacciValid(int n) {
+        if (n < 0) // não existe
+            return false;
+
+        int next = 0, a=0, b=1; // variáveis para encontrar o fibonacci
+
+        while ( a <= n) {
+            if (a == n)  // valor encontrado
+                return true;
+            next = a + b;
+            a = b;
+            b = next;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o número que deseja verifcar se pertence a sequencia de fibonacci: ");
+        System.out.print("num: ");
+        int n = sc.nextInt();
+
+        System.out.println(isFibonacciValid(n));
+
+        sc.close();
+    }
+}
